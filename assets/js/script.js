0b11c7884fd6bebec5f0e70c013ff70f25f89f31
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2024-04-17 19:14:53 +0100

    feat: add mobile navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// AOS Config
 AOS.init({
   duration: 3000,
   once: true,
 });
 
+// Tailwind Config
 tailwind.config = {
   theme: {
     extend: {
@@ -18,3 +20,25 @@ tailwind.config = {
     },
   },
 };
+
+// Mobile Navigation Menu
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileNavbar = document.getElementById('mobile-navbar');
+  const openButton = document.getElementById('menu-button');
+  const closeButton = document.getElementById('close-button');
+  const navLinks = document.querySelectorAll('#mobile-navbar a');
+
+  openButton.addEventListener('click', function () {
+    mobileNavbar.style.transform = 'translateX(0)';
+  });
+
+  closeButton.addEventListener('click', function () {
+    mobileNavbar.style.transform = 'translateX(100%)';
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', function () {
+      mobileNavbar.style.transform = 'translateX(100%)';
+    });
+  });
+});
