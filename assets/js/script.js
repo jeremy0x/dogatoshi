8552b26114ed3efaeb0643960ad4661755b7354d
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2024-04-17 18:12:19 +0100

    feat: add page footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 AOS.init({
   duration: 3000,
   once: true,
-  mobile: false,
 });
 
 tailwind.config = {
@@ -11,6 +10,7 @@ tailwind.config = {
         'hero-pattern': "url('./assets/images/hero-pattern.png')",
         'features-bg-pattern': "url('./assets/images/features-bg-pattern.png')",
         'gradient-pattern': "url('./assets/images/gradient-pattern.png')",
+        'footer-bg-pattern': "url('./assets/images/footer-bg-pattern.png')",
       },
       fontFamily: {
         'titan-one': ['Titan One', 'sans-serif'],
